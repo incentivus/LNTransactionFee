64cccae3ec3258fb047191e472bc49d38ef764cf
kiana Asgari <58818331+Kiana-Asgari@users.noreply.github.com>
2022-08-11 09:58:51 +0200

    Update ln_fee.py

--- scripts/ln_fee.py
+++ scripts/ln_fee.py
@@ -28,10 +28,10 @@ def main():
     parser.add_argument('--fee_base_upper_bound', type=int, default=10000)
     parser.add_argument('--total_timesteps', type=int, default=1000000)
     parser.add_argument('--max_episode_length', type=int, default=200)
-    parser.add_argument('--local_size', type=int, default=100)
-    parser.add_argument('--counts', default=[10, 10, 10])
-    parser.add_argument('--amounts', default=[10000, 50000, 100000])
-    parser.add_argument('--epsilons', default=[.6, .6, .6])
+    parser.add_argument('--local_size', type=int, default=100)  
+    parser.add_argument('--counts', default=[10, 10, 10], type=lambda s: [int(item) for item in s.split(',')])
+    parser.add_argument('--amounts', default=[10000, 50000, 100000], type=lambda s: [int(item) for item in s.split(',')])
+    parser.add_argument('--epsilons', default=[.6, .6, .6], type=lambda s: [float(item) for item in s.split(',')])
     parser.add_argument('--device', default='auto')
 
     
